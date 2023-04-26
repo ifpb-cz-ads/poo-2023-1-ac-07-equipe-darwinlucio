34b26a11976be435877a514057dcffc215485b9e
João Darwin <107586789+Joao-Darwin@users.noreply.github.com>
2023-04-26 10:04:49 -0300

    Create AC7CBBAtividadePratica02.java

---
+++ AC7CBBAtividadePratica02.java
@@ -0,0 +1,10 @@
+public class AC7CBBAtividadePratica02 {
+	public static void main(String[] args) {
+		int tamanho = 100;
+		int[] numeros = new int[tamanho];
+		for(int i = 0; i < tamanho; i++) {
+			numeros[i] = i+1;
+			System.out.println(numeros[i]);
+		}
+	}
+}
